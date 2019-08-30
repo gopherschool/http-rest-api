8336a07a55d34877b58c1b7edb5750b266ad86aa
Alexander Tipugin <atipugin@gmail.com>
2019-08-30 15:11:06 +0300

    Simplify server struct, use standalone Start func to start http server

---
+++ internal/app/apiserver/server.go
@@ -0,0 +1,32 @@
+package apiserver
+
+import (
+	"net/http"
+
+	"github.com/gopherschool/http-rest-api/internal/app/store"
+	"github.com/gorilla/mux"
+)
+
+type server struct {
+	router *mux.Router
+	store  store.Store
+}
+
+func newServer(store store.Store) *server {
+	s := &server{
+		router: mux.NewRouter(),
+		store:  store,
+	}
+
+	s.configureRouter()
+
+	return s
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
+}
+
+func (s *server) configureRouter() {
+	// ...
+}
