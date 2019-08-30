b2f572a452eb4526a9bcc0aefdfff86d201c5be6
Alexander Tipugin <atipugin@gmail.com>
2019-08-30 14:32:03 +0300

    Rename `store` => `sqlstore`

--- internal/app/store/sqlstore/userrepository.go
+++ internal/app/store/sqlstore/userrepository.go
@@ -1,4 +1,4 @@
-package store
+package sqlstore
 
 import "github.com/gopherschool/http-rest-api/internal/app/model"
 
--- internal/app/store/sqlstore/userrepository_test.go
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -1,28 +1,28 @@
-package store_test
+package sqlstore_test
 
 import (
 	"testing"
 
 	"github.com/gopherschool/http-rest-api/internal/app/model"
-	"github.com/gopherschool/http-rest-api/internal/app/store"
+	"github.com/gopherschool/http-rest-api/internal/app/store/sqlstore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
 
-	s := store.New(db)
+	s := sqlstore.New(db)
 	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
 
-	s := store.New(db)
+	s := sqlstore.New(db)
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
