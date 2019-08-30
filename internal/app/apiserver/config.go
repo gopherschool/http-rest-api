173b761d7fa3e82300f5948e90d1194a9f6e5155
Alexander Tipugin <atipugin@gmail.com>
2019-08-30 15:05:18 +0300

    Simplify apiserver.Config

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,12 +1,10 @@
 package apiserver
 
-import "github.com/gopherschool/http-rest-api/internal/app/store"
-
 // Config ...
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	LogLevel string `toml:"log_level"`
-	Store    *store.Config
+	BindAddr    string `toml:"bind_addr"`
+	LogLevel    string `toml:"log_level"`
+	DatabaseURL string `toml:"database_url"`
 }
 
 // NewConfig ...
@@ -14,6 +12,5 @@ func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		LogLevel: "debug",
-		Store:    store.NewConfig(),
 	}
 }
